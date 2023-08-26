0a7736dd6cfa63f4514846a9620c3457d79bf872
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-08-26 20:40:12 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ProjectJoinComponent } from './project-join/project-join.component';
+import { ProjectElPolloLocoComponent } from './project-el-pollo-loco/project-el-pollo-loco.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    MyskillsComponent,
+    PortfolioComponent,
+    ProjectJoinComponent,
+    ProjectElPolloLocoComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
